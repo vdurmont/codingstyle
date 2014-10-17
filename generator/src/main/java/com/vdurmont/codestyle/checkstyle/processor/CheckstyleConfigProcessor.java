c35b46090d188bb0a83cd2c3495a45e44710aa7c
Vincent DURMONT <vdurmont@gmail.com>
2014-10-16 19:53:14 -0700

    Configuration of the JAXB processor for the Checkstyle so that it would be compliant to the DTD

--- generator/src/main/java/com/vdurmont/codestyle/checkstyle/processor/CheckstyleConfigProcessor.java
+++ generator/src/main/java/com/vdurmont/codestyle/checkstyle/processor/CheckstyleConfigProcessor.java
@@ -3,13 +3,18 @@ package com.vdurmont.codestyle.checkstyle.processor;
 import com.vdurmont.codestyle.checkstyle.model.Checkstyle;
 import com.vdurmont.codestyle.core.processor.JaxbConfigProcessor;
 
+import javax.xml.bind.Marshaller;
+
 public class CheckstyleConfigProcessor extends JaxbConfigProcessor<Checkstyle> {
-    private static final String XML_HEADERS = "<!DOCTYPE module PUBLIC\n"
+    private static final String XML_HEADERS = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n\n"
+            + "<!DOCTYPE module PUBLIC\n"
             + "    \"-//Puppy Crawl//DTD Check Configuration 1.2//EN\"\n"
-            + "    \"http://www.puppycrawl.com/dtds/configuration_1_2.dtd\">";
+            + "    \"http://www.puppycrawl.com/dtds/configuration_1_2.dtd\">\n";
 
     public CheckstyleConfigProcessor() {
         super(Checkstyle.class);
+        this.addProperty(Marshaller.JAXB_FRAGMENT, true);
         this.addProperty("com.sun.xml.internal.bind.xmlHeaders", XML_HEADERS);
+        this.addProperty("com.sun.xml.internal.bind.indentString", "\t");
     }
 }
